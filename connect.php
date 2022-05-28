f0a3954583ef6b7d7f739aea8c4f5f354c447998
GioDelaPena <90368122+GioDelaPena@users.noreply.github.com>
2022-05-28 17:02:45 +0800

    Answer

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+    $con = mysqli_connect("localhost", "root", "", "reservation");
+
+    if(!$con){
+        die("Registration Failed! : ".mysqli_connect_error());
+    }
+    else{
+        echo("Successfuly Registered!");
+    }
+
+?>
